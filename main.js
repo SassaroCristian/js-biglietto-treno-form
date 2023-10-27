df3fba9df3f3bf80cf7ab4d9441f9291d30ee245
Zazzaro <sassaro01cristian@gmail.com>
2023-10-27 20:28:04 +0200

    added a delete button and components

--- main.js
+++ main.js
@@ -3,15 +3,10 @@ function createTicket() {
     const fullNameValue = document.getElementById('fullName').value;
     const distanceValue = parseInt(document.getElementById('distance').value);
     const age = document.getElementById('fasciaEta').value;
-    
-   
     const priceOneKilometer = 0.21;
     let priceKilometer = distanceValue * priceOneKilometer;
-
-    
     const under18sale = priceKilometer * 0.20; 
     const over65sale = priceKilometer * 0.40; 
-
     if (age == 'noSale') {
         priceKilometer;
     } else if (age == 'under18'){
@@ -19,11 +14,11 @@ function createTicket() {
     } else if (age == 'over65') {
         priceKilometer -=over65sale;
     }
-
-   
-    const ticket = `Nome e cognome: ${fullNameValue}, Costo biglietto: ${priceKilometer}`;
-    
-    
+    const ticket = `Nome e cognome: ${fullNameValue}, Costo biglietto: ${priceKilometer.toFixed(2)}`;
     const ticketOutput = document.getElementById('ticketOutput');
     ticketOutput.textContent = ticket;
+}
+
+function deleteTicket() {
+    location.reload();
 }
