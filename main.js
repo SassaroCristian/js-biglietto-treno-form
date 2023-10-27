4688f2327a452e9a1b358d58f8844f589751005c
Zazzaro <sassaro01cristian@gmail.com>
2023-10-27 19:46:18 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+function createTicket() {
+
+    const fullNameValue = document.getElementById('fullName').value;
+    const distanceValue = parseInt(document.getElementById('distance').value);
+    const age = document.getElementById('fasciaEta').value;
+    
+   
+    const priceOneKilometer = 0.21;
+    let priceKilometer = distanceValue * priceOneKilometer;
+
+    
+    const under18sale = priceKilometer * 0.20; 
+    const over65sale = priceKilometer * 0.40; 
+
+    if (age == 'noSale') {
+        priceKilometer;
+    } else if (age == 'under18'){
+        priceKilometer -= under18sale;
+    } else if (age == 'over65') {
+        priceKilometer -=over65sale;
+    }
+
+   
+    const ticket = `Nome e cognome: ${fullNameValue}, Costo biglietto: ${priceKilometer}`;
+    
+    
+    const ticketOutput = document.getElementById('ticketOutput');
+    ticketOutput.textContent = ticket;
+}
